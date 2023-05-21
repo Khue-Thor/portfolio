7fcf66de4fbd5350977bbd9abd0a030101759add
khuephamy <khuephumy19@gmail.com>
2023-05-21 09:46:20 -0500

    herobg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,13 @@
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 const Hero = () => {
-  return <section className="relative w-full h-screen max-auto">hero</section>;
+  return (
+    <section className="relative w-full h-screen max-auto">
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl max-auto flex flex-row items-start gap-5`}
+      ></div>
+    </section>
+  );
 };
 
 export default Hero;
